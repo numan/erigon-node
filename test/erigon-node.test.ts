54ccfa449f4858c75cde8d284c924cac6cbc7822
numan <numan856@gmail.com>
2022-02-07 13:52:14 -0800

    Initial commit

---
+++ test/erigon-node.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ErigonNode from '../lib/erigon-node-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/erigon-node-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ErigonNode.ErigonNodeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
